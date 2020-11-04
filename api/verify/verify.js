a050225430bf56e79571024f3055349448a1f850
xzf1998 <867932074@qq.com>
2020-11-04 10:20:23 +0800

    verify删除了res，可以让经过验证的后端api向前端回复消息

--- api/verify/verify.js
+++ api/verify/verify.js
@@ -5,22 +5,19 @@ var verify = function (req, res, next) {
 	//利用请求头得到携带的token信息
 	var token = req.headers['authorization'];
 	// var token = 12345;//检验判断token错误
-	if (token == 'null' || token == undefined) {
+	if (token == 'null') {
 		console.log('token未定义');
-		res.json('token未定义');
 		next();
 	}
 	//token不为空才验证
 	else {
 		//调用token的验证函数，如果验证成功，则用data获取数据
 		tokenPackage.getToken(token).then((data) => {
-			res.json('token验证成功');
 			console.log('token验证成功');
 			next();
 		}).catch((error) => {
 			console.log('ERROR', error);
 			console.log('执行错误');
-			res.json('token码错误');
 			next();
 		})
 	}
